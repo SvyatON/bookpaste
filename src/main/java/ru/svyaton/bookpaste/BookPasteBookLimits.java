21c9788eea7a9e0dfb4ed81ff3ab95adbf260809
SvyatON <SvyatON@users.noreply.github.com>
2026-07-05 17:25:29 +0300

    Remove shared src/main (standaloneSources)

--- src/main/java/ru/svyaton/bookpaste/BookPasteBookLimits.java
+++
@@ -1,9 +0,0 @@
-package ru.svyaton.bookpaste;
-
-public final class BookPasteBookLimits {
-    public static final int MAX_PAGE_COUNT = 100;
-    public static final int MAX_PAGE_LENGTH = 1024;
-
-    private BookPasteBookLimits() {
-    }
-}
